45ac8e3f5af7dc3eb068bd6a2aeafb6d98113f01
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2024-05-15 00:41:37 +0600

    initial commit

---
+++ src/utils/BoilerPlate.java
@@ -0,0 +1,55 @@
+package utils;
+
+
+// Replace class name with your topic
+class BoilerPlate {
+
+    //Define Approaches
+    enum Approach {
+        FIRST_APPROACH,
+        SECOND_APPROACH,
+        THIRD_APPROACH,
+    }
+
+    // Input the approach you want to execute.
+    private static final Approach approach = Approach.SECOND_APPROACH;
+
+
+    // Maintain the actual method name for clear understandings
+    public int[] your_method_name(int[] your_inputs) {
+        if (approach.equals(Approach.FIRST_APPROACH)) {
+            return Approach_Your_Approach_Number(your_inputs);
+        } else if (approach.equals(Approach.SECOND_APPROACH)) {
+            return Approach_Your_Approach_Number(your_inputs);
+        } else if (approach.equals(Approach.THIRD_APPROACH)) {
+            return Approach_Your_Approach_Number(your_inputs);
+        }
+        return null;
+    }
+
+    // Approach_Your_Approach_Number: Approach Name with Time and Space Complexity
+    // Outcome: Outcome of your solution (i.e., ACCEPTED, TLE, FAILED, COMPILATION ERROR, RUNTIME ERROR)
+    /*
+     ---------------------------------------------------------------------- NOTE ----------------------------------------------------------------------
+     * Make a short note on your approach
+    */
+
+    /*
+     ---------------------------------------------------------------------- Algorithm ----------------------------------------------------------------------
+     * Write an algorithm on your approach.
+    */
+    private int[] Approach_Your_Approach_Number(int[] your_inputs) {
+        // Code starts from here.
+        return your_inputs;
+    }
+
+    // Driver Class
+    public static class Driver {
+        public static void main(String[] args) {
+            // Code starts from here.
+        }
+    }
+}
+
+// Conclusion: Compare your approaches and report the best one according to your findings.
+// FUTURE PLAN: Write about your upcoming approaches on this problem.
